e46c50b95ce63869dd2b3d17c6d14f251ba724fa
Conji <dubstepandponies95@gmail.com>
2014-10-31 18:52:55 -0700

    Initial (but a lot of it)

---
+++ Volatile.Db/DatabaseObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volatile.Db
+{
+    public struct DatabaseObject<T>
+    {
+        public string Key;
+        public T Value;
+
+        public DatabaseObject(string key, T value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+
+    public struct DatabaseObject
+    {
+        public string Key;
+        public object Value;
+
+        public DatabaseObject(string key, object value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
